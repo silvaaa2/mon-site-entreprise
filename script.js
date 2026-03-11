0eb3e8790b4618424dd67dada4cc516c134e854e
silvaaa2 <dr947695@gmail.com>
2026-03-11 12:26:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+// Petit script pour gérer le clic fluide sur les liens (Smooth Scroll)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Pour plus tard : ici tu pourras ajouter la logique d'un menu déroulant sur mobile !
